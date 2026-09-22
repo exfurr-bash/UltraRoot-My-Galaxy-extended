7552a79e52d5971ed203ff30df8d406189487e5a
Root My Galaxy <root-my-galaxy@local>
2026-09-21 22:09:58 -0500

    Bump version to 0.2.68 (code 16)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dev.busung.s25uroot"
         minSdk = 33
         targetSdk = 36
-        versionCode = 15
-        versionName = "0.2.67"
+        versionCode = 16
+        versionName = "0.2.68"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
